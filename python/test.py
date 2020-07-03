ad3883bc04a3a7eeed092c5d29335dd5931058c8
phanh842002 <phanh842002>
2020-07-03 22:38:31 +1000

    testing 2

---
+++ python/test.py
@@ -0,0 +1,13 @@
+canadaDate = "July 1st"
+print("Canada day is on " + canadaDate)
+australiaDate = "July 1st"
+print("Australia day is on " + australiaDate)
+num = 116
+print("The oldest person alive is Emma Morano she is " + str(num))
+print("The oldest person alive is Emma Morano \t \rShe is " + str(num) )
+print("\t \t \t \t \t \t She is " + str(num))
+print("Please enter three numbers.")
+num1 = int(input("Please enter number 1:"))
+num2 = int(input("Please enter number 2:"))
+num3 = int(input("Please enter number 3:"))
+print("The sum of these number is:", (num1 + num2 + num3))
